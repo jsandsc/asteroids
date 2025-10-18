d066e221e3d939c7bbd67712989188b9e1afc737
cjsands <colby.sandstrom08@gmail.com>
2025-10-18 14:20:42 -0700

    Created CircleShape and Player classes

--- main.py
+++ main.py
@@ -3,9 +3,15 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -17,10 +23,14 @@ def main():
                 return
 
         screen.fill((0,0,0))
+        player.draw(screen)
 
         pass
 
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+
 
 
 
